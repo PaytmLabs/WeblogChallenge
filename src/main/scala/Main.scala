8e20f6eb99448564da63785177f17b547f5ba595
Mehrdad_Niasari <Arabpour.m@gmail.com>
2018-06-05 08:39:44 -0400

    Added Sessionized

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,14 +4,19 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.types.{StructType, StructField}
 import org.apache.spark.sql.types.{TimestampType, StringType, DoubleType, LongType}
 
+import org.apache.spark.sql.expressions.Window
+
+import org.apache.spark.sql.functions._
+
 object Main {
 
-  // columns based on: http://docs.aws.amazon.com/ElasticLoadBalancing/latest/DeveloperGuide/access-log-collection.html#access-log-entry-format
+  val INACTIVE_TIME_THRESHOLD_MIN = 15
+  // original columns, based on: http://docs.aws.amazon.com/ElasticLoadBalancing/latest/DeveloperGuide/access-log-collection.html#access-log-entry-format
 
   val TIMESTAMP_COL = "timestamp"
   val ELB_COL = "elb"
-  val CLIENT_IP_COL = "client_ip"
-  val BACKEND_IP_COL = "backend_ip"
+  val CLIENT_IP_PORT_COL = "client_ip_port"
+  val BACKEND_IP_PORT_COL = "backend_ip_port"
   val REQUEST_TIME_COL = "request_time"
   val BACKEND_TIME_COL = "backend_time"
   val RESPONSE_TIME_COL = "response_time"
@@ -24,11 +29,16 @@ object Main {
   val SSL_CIPHER_COL = "ssl_cipher"
   val SSL_PROTOCOL_COL = "ssl_protocol"
 
+
+  // derived columns
+  val SESSION_ID_COL = "session_id"
+  val SESSION_DURATION = "session_duration"
+
   val schema = StructType(Array(
     StructField(TIMESTAMP_COL, TimestampType, true),
     StructField(ELB_COL, StringType, true),
-    StructField(CLIENT_IP_COL, StringType, true),
-    StructField(BACKEND_IP_COL, StringType, true),
+    StructField(CLIENT_IP_PORT_COL, StringType, true),
+    StructField(BACKEND_IP_PORT_COL, StringType, true),
     StructField(REQUEST_TIME_COL, DoubleType, true),
     StructField(BACKEND_TIME_COL, DoubleType, true),
     StructField(RESPONSE_TIME_COL, DoubleType, true),
@@ -40,9 +50,7 @@ object Main {
     StructField(USER_AGENT_COL, StringType, true),
     StructField(SSL_CIPHER_COL, StringType, true),
     StructField(SSL_PROTOCOL_COL, StringType, true)
-  )
-
-  )
+  ))
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("Weblog Challenge").master("local[*]").getOrCreate()
@@ -51,7 +59,13 @@ object Main {
     val inputPath = args(0)
     val rawDf = readLogs(spark, inputPath)
 
-    rawDf.show(5, false)
+    rawDf.printSchema()
+    val sessionizedDf = sessionize(rawDf)
+    sessionizedDf.cache()
+
+    sessionizedDf.show(10, false)
+    println(sessionizedDf.count)
+    sessionizedDf.sort(desc(SESSION_DURATION)).show(10, false)
   }
 
   def readLogs(spark: SparkSession, inputPath: String): DataFrame = {
@@ -61,6 +75,33 @@ object Main {
       .option("header", false)
       .schema(schema)
       .csv(inputPath)
+  }
+
+  def sessionize(df: DataFrame): DataFrame = {
+    val CLIENT_BROWSER_COL = "client_browser"
+    val IS_NEW_SESSION_COL = "is_new_session"
+    val TIMESTAMP_LONG_COL = "timestamp_long"
+
+    val getBrowserUdf = udf((msg: String) => (
+      if (msg != null && msg.contains(' '))
+        msg.split(' ')(0).trim
+      else "unknown"
+      ))
+
+    val timeOrderedWin = Window.partitionBy(CLIENT_IP_PORT_COL, CLIENT_BROWSER_COL).orderBy(TIMESTAMP_LONG_COL)
+
+    val dfWithSessionId = df
+      .withColumn(TIMESTAMP_LONG_COL, col(TIMESTAMP_COL).cast("long"))
+      .withColumn(CLIENT_BROWSER_COL, getBrowserUdf(col(USER_AGENT_COL)))
+      .withColumn(IS_NEW_SESSION_COL,
+        (col(TIMESTAMP_LONG_COL) - coalesce(lag(col(TIMESTAMP_LONG_COL), 1).over(timeOrderedWin), lit(0)) > INACTIVE_TIME_THRESHOLD_MIN * 60).cast("long"))
+      .withColumn(SESSION_ID_COL, sum(IS_NEW_SESSION_COL).over(timeOrderedWin))
+      .drop(IS_NEW_SESSION_COL)
+
+    val sessionizedDf = dfWithSessionId.groupBy(CLIENT_IP_PORT_COL, CLIENT_BROWSER_COL, SESSION_ID_COL)
+      .agg((max(TIMESTAMP_LONG_COL) - min(TIMESTAMP_LONG_COL)).alias(SESSION_DURATION))
+
+    sessionizedDf
   }
 
 }
