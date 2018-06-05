ba31c2a1909c341fbc50d7db8756c5a120661932
Mehrdad Niasari <mehrdad.niasari@rbc.com>
2018-06-05 17:09:53 -0400

    Added most engaged ips and average session time

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -33,6 +33,7 @@ object Main {
   // derived columns
   val SESSION_ID_COL = "session_id"
   val SESSION_DURATION = "session_duration"
+  val TOTAL_SESSION_DURATION = "total_session_duration"
 
   val schema = StructType(Array(
     StructField(TIMESTAMP_COL, TimestampType, true),
@@ -64,8 +65,18 @@ object Main {
     sessionizedDf.cache()
 
     sessionizedDf.show(10, false)
-    println(sessionizedDf.count)
-    sessionizedDf.sort(desc(SESSION_DURATION)).show(10, false)
+
+
+    println(s"Average of All Sessions Time:")
+    sessionizedDf.agg(avg(col(SESSION_DURATION))).show(false)
+
+    // There could possibly be more than one ip with max duration time
+    val mostEngagedIpsDf: DataFrame = findMostEngagedIPs(sessionizedDf)
+    println(s"number of most engaged IPs: ${mostEngagedIpsDf.count}")
+    mostEngagedIpsDf.show()
+
+
+    spark.close()
   }
 
   def readLogs(spark: SparkSession, inputPath: String): DataFrame = {
@@ -78,6 +89,11 @@ object Main {
   }
 
   def sessionize(df: DataFrame): DataFrame = {
+    /*
+      Assumptions:
+        - We don't care about session_time = 0 sessions
+     */
+
     val CLIENT_BROWSER_COL = "client_browser"
     val IS_NEW_SESSION_COL = "is_new_session"
     val TIMESTAMP_LONG_COL = "timestamp_long"
@@ -94,14 +110,27 @@ object Main {
       .withColumn(TIMESTAMP_LONG_COL, col(TIMESTAMP_COL).cast("long"))
       .withColumn(CLIENT_BROWSER_COL, getBrowserUdf(col(USER_AGENT_COL)))
       .withColumn(IS_NEW_SESSION_COL,
-        (col(TIMESTAMP_LONG_COL) - coalesce(lag(col(TIMESTAMP_LONG_COL), 1).over(timeOrderedWin), lit(0)) > INACTIVE_TIME_THRESHOLD_MIN * 60).cast("long"))
+        (col(TIMESTAMP_LONG_COL) - (lag(col(TIMESTAMP_LONG_COL), offset = 1, defaultValue = 0).over(timeOrderedWin)) > INACTIVE_TIME_THRESHOLD_MIN * 60).cast("long"))
       .withColumn(SESSION_ID_COL, sum(IS_NEW_SESSION_COL).over(timeOrderedWin))
       .drop(IS_NEW_SESSION_COL)
 
     val sessionizedDf = dfWithSessionId.groupBy(CLIENT_IP_PORT_COL, CLIENT_BROWSER_COL, SESSION_ID_COL)
       .agg((max(TIMESTAMP_LONG_COL) - min(TIMESTAMP_LONG_COL)).alias(SESSION_DURATION))
+      .filter(col(SESSION_DURATION) =!= 0)
 
     sessionizedDf
+  }
+
+  def findMostEngagedIPs(sessionizedDf: DataFrame): DataFrame ={
+    val sessionsTimeByIpDf = sessionizedDf.groupBy(CLIENT_IP_PORT_COL)
+      .agg(sum(SESSION_DURATION).alias(TOTAL_SESSION_DURATION)).cache()
+
+    val longestSessionTime: Long = sessionsTimeByIpDf.select(max(col(TOTAL_SESSION_DURATION))).take(1).head.getAs[Long](0)
+    val mostEngagedIpsDf = sessionsTimeByIpDf.filter(col(TOTAL_SESSION_DURATION) === longestSessionTime)
+      .select(CLIENT_IP_PORT_COL)
+
+
+    mostEngagedIpsDf
   }
 
 }
